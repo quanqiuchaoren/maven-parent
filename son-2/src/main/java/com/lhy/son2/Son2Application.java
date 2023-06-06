1b8f48d67ec4d5b01904eab3207c4b8c2ec2fccd
quanqiuchaoren <quanqiuchaoren@gmail.com>
2023-06-06 23:57:27 +0800

    初始化

---
+++ son-2/src/main/java/com/lhy/son2/Son2Application.java
@@ -0,0 +1,11 @@
+package com.lhy.son2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Son2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Son2Application.class);
+    }
+}
--- son-2/src/main/java/com/lhy/son2/Son2Controller.java
+++ son-2/src/main/java/com/lhy/son2/Son2Controller.java
@@ -1,12 +1,15 @@
-package com.example.lhy;
+package com.lhy.son2;
 
+import com.alibaba.fastjson.JSON;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @RestController
-public class Son1Controller {
+public class Son2Controller {
     @RequestMapping("/getName")
     public String getName() {
-        return "son1";
+        return JSON.toJSONString(Arrays.asList("son2", "son2"));
     }
 }
