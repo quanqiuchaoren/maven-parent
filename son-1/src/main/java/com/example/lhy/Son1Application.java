bb60b9c7f1ba97a50cc3f8116a5660404c48a704
quanqiuchaoren <quanqiuchaoren@gmail.com>
2023-06-06 23:21:18 +0800

    初始化

---
+++ son-1/src/main/java/com/example/lhy/Son1Application.java
@@ -0,0 +1,11 @@
+package com.example.lhy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Son1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Son1Application.class);
+    }
+}
---
+++ son-1/src/main/java/com/example/lhy/Son1Controller.java
@@ -0,0 +1,7 @@
+package com.example.lhy;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+public class Son1Controller {
+}
